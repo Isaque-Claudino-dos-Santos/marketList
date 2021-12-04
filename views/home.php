6f63219ed617429580da2af934aeeb0325d67b8e
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2021-12-04 13:48:08 -0500

    duas pagina para test

---
+++ views/home.php
@@ -0,0 +1,26 @@
+<?php
+require_once('./apis/function.php');
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+   <header>
+       <div class="container">
+           <div class="title">
+               <h1>Home</h1>
+           </div>
+           <nav>
+               <ul>
+                   <li><a href='<?php echo route('registerProducts') ?>'>Register Products</a></li>
+               </ul>
+           </nav>
+       </div>
+   </header> 
+</body>
+</html>
---
+++ views/registerProducts.php
@@ -0,0 +1,39 @@
+<?php
+require_once('./apis/function.php');
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo linkCss('reset.css')?>">
+    <link rel="stylesheet" href="<?php echo linkCss('registerProducts.css')?>">
+    <title>Register Products</title>
+</head>
+<body>
+    <main>
+        <div class="title">
+            <h1>Register Products</h1>
+            <button><a href="<?php echo route('home')?>">Goto Home</a></button>
+        </div>
+        <form method="POST">
+            <label for="name">Name</label>
+            <input type="text" id="name" name="name"/>
+            
+            <label for="price">Price</label>
+            <input type="text" id="price" name="price"/>
+
+            <select id="category" name="category">
+                <option value="" hidden selected>Category</option>
+                <option value="drink">Drink</option>
+                <option value="vegetable">Vegetable</option>
+                <option value="grains">Grains</option>
+            </select>
+            <button>Register</button>
+        </form>
+    </main>
+</body>
+</html>
