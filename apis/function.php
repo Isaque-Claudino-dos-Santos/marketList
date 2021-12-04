9350848290f8392b597186834d72777cad4d5ad5
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2021-12-04 13:45:27 -0500

    funções globais do projeto

---
+++ apis/function.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Http\Router;
+require_once('./bootstrep.php');
+
+
+function dd($res,$die = false) {
+    echo '<pre>';
+
+    print_r($res);
+    
+    echo '</pre>';
+    if($die == true) $die;
+} 
+
+function instanciarClass($defaultPath) {
+    $files = scandir($defaultPath);
+    
+    foreach($files as $file) {
+        if($file != '.' && $file != '..') {
+            require_once './'.$defaultPath.'/'.$file;
+           $class = (array_filter(explode('.php',$file))); 
+           return $class;
+        }
+    }
+}
+
+function route($name) {
+    foreach(Router::$name as $key => $url){
+        if($name == $key) {
+            return $url;
+        }
+    };
+}
+
+function linkCss($file) {
+    $path = '/views/css/';
+    return 'http://'.URL_HOST.':'.URL_PORT.$path.$file;
+}
