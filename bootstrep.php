dda96e30b14263b91a671ab8f11e3dcdafa45887
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2021-12-04 13:43:56 -0500

    variaveis de ambiente

---
+++ bootstrep.php
@@ -0,0 +1,11 @@
+<?php
+
+define('DB_HOST','localhost');
+define('DB_NAME','marketList');
+define('DB_PORT',3306);
+define('DB_USER','isaque');
+define('DB_PASS','atecubanos');
+
+define('URL_HOST', 'localhost');
+define('URL_PORT', '1223');
+
