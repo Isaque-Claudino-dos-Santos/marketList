e9c6cbb029697f6a4cca384c2ce928d691967bf7
Isaque dos Santos <74436802+Isaque-Claudino-dos-Santos@users.noreply.github.com>
2021-12-10 17:17:50 -0300

    Update bootstrep.php

--- bootstrep.php
+++ bootstrep.php
@@ -4,7 +4,7 @@ define('DB_HOST','localhost');
 define('DB_NAME','marketList');
 define('DB_PORT',3306);
 define('DB_USER','isaque');
-define('DB_PASS','atecubanos');
+define('DB_PASS','');
 
 define('URL_HOST', 'localhost');
 define('URL_PORT', '1223');
